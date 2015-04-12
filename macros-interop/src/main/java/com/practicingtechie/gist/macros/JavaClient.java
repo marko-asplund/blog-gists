8f70c0af2525c27fd958ed4d5ba0a17747e67b8f
marko asplund <marko.asplund@gmail.com>
2015-04-12 22:36:41 +0300

    macros-interop: demostrate failure case

--- macros-interop/src/main/java/com/practicingtechie/gist/macros/JavaClient.java
+++ macros-interop/src/main/java/com/practicingtechie/gist/macros/JavaClient.java
@@ -3,6 +3,6 @@ package com.practicingtechie.gist.macros;
 public class JavaClient {
   public static void main(String ... args) {
     MyReusableService.regularMethod();
-//    MyReusableService.macroMethod();
+    MyReusableService.macroMethod();
   }
 }
